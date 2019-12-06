ba889bd638c35b81ffb11e6128db256ffac7e87f
Cody Shepherd <cody.shepherd@canonical.com>
2019-12-05 23:18:47 -0800

    Day 6 Part 2

--- 6.py
+++ 6.py
@@ -1,3 +1,25 @@
+'''
+Advent of Code 2019
+Cody Shepherd
+
+Day 6, Parts 1 & 2
+
+Part 1 Solution:
+
+This was pretty easy -- just construct a digraph, count the path from each
+node to COM.
+
+Part 2 Solution:
+
+This is a simple BFS problem, which requires an undirected graph. DFS would be
+fine here as well, since we have no danger of loops or infinite branches, but
+I wanted to use something different than recursion (though ofc that wouldn't be
+strictly necessary).
+'''
+
+import collections
+import queue
+
 from typing import(
     Dict,
     List,
@@ -5,6 +27,7 @@ from typing import(
 
 INPUT_FILE = 'input_6.txt'
 
+GRAPH = None
 O_I_EDGES = None
 
 def all_edges(key: str) -> int:
@@ -15,6 +38,38 @@ def all_edges(key: str) -> int:
         next_planet = O_I_EDGES[next_planet]
 
     return num
+
+def bfs(yours: str, santas: str) -> int:
+    xfers = 0
+    q = queue.Queue()
+    visited = {
+        yours: True
+    }
+    yr_edges = GRAPH[yours]
+    for edge in yr_edges:
+        q.put((edge, xfers+1))
+
+    while not q.empty():
+        next_p, xfers = q.get()
+        if next_p == santas:
+            return xfers
+        visited[next_p] = True
+        edges_p = GRAPH[next_p]
+        for edge in edges_p:
+            if visited.get(edge, None) is None:
+                q.put((edge, xfers+1))
+    return -1
+
+    
+def map_graph(raw_list: List[str]) -> Dict[str, List[str]]:
+    edges = collections.defaultdict(list)
+    for item in raw_list:
+        planets = item.split(')')
+        inner = planets[0]
+        outer = planets[1]
+        edges[inner].append(outer)
+        edges[outer].append(inner)
+    return edges
 
 def map_outer_to_inner(raw_list: List[str]) -> Dict[str, str]:
     edges = {}
@@ -36,3 +91,10 @@ if __name__ == '__main__':
         num_edges += all_edges(k)
 
     print(f'num edges: {num_edges}')
+
+    GRAPH = map_graph(raw_list)
+
+    yours = O_I_EDGES['YOU']
+    santas = O_I_EDGES['SAN']
+    xfers = bfs(yours, santas)
+    print(f'xfers: {xfers}')
